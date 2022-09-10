a6f7d9932ca85531dc8efebf5fc67933c9b0c63d
KevinSenjaya937 <82190644+KevinSenjaya937@users.noreply.github.com>
2022-09-10 17:37:21 +0800

    Add project files.

---
+++ BusinessLayerAPI/Controllers/GenerateDataController.cs
@@ -0,0 +1,53 @@
+﻿using DC_Practical_5.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BusinessLayerAPI.Controllers
+{
+    [RoutePrefix("api/data")]
+    public class GenerateDataController : ApiController
+    {
+        private static string URL = "http://localhost:56653/";
+        private static RestClient client = new RestClient(URL);
+        Random rand = new Random();
+        private string[] firstnames = { "Jack", "Chris", "Matthew", "Ryan", "Michael", "Steven" };
+        private string[] lastnames = { "Love", "Brown", "Green", "McCarter", "O'Brian", "Griffith" };
+        private uint[] pins = { 1111, 2222, 3333, 4444, 5555, 6666 };
+        private uint[] acctNos = { 000001, 000002, 000003, 000004, 000005, 000006 };
+        private int[] balances = { -100, 0, 2000, 3000, 50000, 100000 };
+
+        [Route("generate")]
+        public void GenerateData()
+        {
+            RestRequest request = new RestRequest("api/customer/post");
+            Customer customer = new Customer();
+
+            for (int i = 0; i < 100; i++)
+            {
+                customer = GetNextAccount(customer);
+                request.AddJsonBody(customer);
+                client.Post(request);
+            }
+        }
+
+        public Customer GetNextAccount(Customer customer)
+        {
+            customer.Pin_Number = pins[GenerateRandNum()].ToString();
+            customer.Account_Number = (int)acctNos[GenerateRandNum()];
+            customer.First_Name = firstnames[GenerateRandNum()];
+            customer.Last_Name = lastnames[GenerateRandNum()];
+            customer.Balance = balances[GenerateRandNum()];
+            return customer;
+        }
+
+        private int GenerateRandNum()
+        {
+            return rand.Next(0, 5);
+        }
+    }
+}
---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string URL = "http://localhost:53590/";
+        private static RestClient client = new RestClient(URL);
+        public MainWindow()
+        {
+            InitializeComponent();
+            RestRequest request = new RestRequest("api/data/generate");
+        }
+    }
+}
